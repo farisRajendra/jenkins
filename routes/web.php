52b2907a0b30fb302f1eb32778bcf12e910b51b3
faris rajendra maulana <125250920+farisRajendra@users.noreply.github.com>
2025-05-13 21:25:08 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Definisi route yang benar
+Route::get('/', [PageController::class, 'home']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/contact', [PageController::class, 'contact']);
